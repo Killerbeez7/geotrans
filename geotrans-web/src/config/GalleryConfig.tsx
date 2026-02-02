9db45f950df463ca3f648e3be417056483eeb969
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2026-02-02 20:14:43 +0200

    fix img paths

--- geotrans-web/src/config/GalleryConfig.tsx
+++ geotrans-web/src/config/GalleryConfig.tsx
@@ -5,7 +5,7 @@ export interface GalleryImage {
 }
 
 export const GALLERY_IMAGES: GalleryImage[] = [
-  { id: 1, src: "/teren.jpg", alt: "Геодезист с тотална станция на терен" },
-  { id: 2, src: "/tripod.jpg", alt: "Геодезическо оборудване" },
-  { id: 3, src: "/teren.jpg", alt: "Геодезическо заснемане на обект" },
+  { id: 1, src: "/images/teren.jpg", alt: "Геодезист с тотална станция на терен" },
+  { id: 2, src: "/images/tripod.jpg", alt: "Геодезическо оборудване" },
+  { id: 3, src: "/images/teren.jpg", alt: "Геодезическо заснемане на обект" },
 ];
