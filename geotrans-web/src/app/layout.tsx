bcf8847f9183a370c35d8817a093fb54a9608fa2
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2026-02-12 01:57:27 +0200

    feat(Layout): fix imports, add bg color to main

--- geotrans-web/src/app/layout.tsx
+++ geotrans-web/src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import { Navbar } from "../components/Navbar";
-import { Footer } from "../components/Footer";
+import { Navbar } from "@/components/layout/Navbar";
+import { Footer } from "@/components/layout/Footer";
 
 const inter = Inter({
     subsets: ["latin", "cyrillic"],
@@ -20,7 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="bg">
             <body className={`${inter.className} antialiased flex flex-col`}>
                 <Navbar />
-                <main className="grow pt-16">{children}</main>
+                <main className="grow pt-16 bg-(--bg-page)">{children}</main>
                 <Footer />
             </body>
         </html>
