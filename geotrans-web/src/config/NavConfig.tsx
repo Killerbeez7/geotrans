4e91954dc6efbf54db7f00b796ae8bc751b59e76
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2026-02-16 17:56:42 +0200

    refactor(NavConf): add id, change name with label

--- geotrans-web/src/config/NavConfig.tsx
+++ geotrans-web/src/config/NavConfig.tsx
@@ -1,14 +1,14 @@
 export interface NavItem {
-    name: string;
+    id: string;
+    label: string;
     href: string;
     hasDropdown?: boolean;
 }
 
-
-export const NAV_LINKS: (NavItem)[] = [
-    { name: "Начало", href: "/" },
-    { name: "Услуги", href: "/services", hasDropdown: true},
-    { name: "Проекти", href: "/projects" },
-    { name: "За нас", href: "/about" },
-    { name: "Контакти", href: "/contacts" },
+export const NAV_LINKS: NavItem[] = [
+    { id: "home", label: "Начало", href: "/" },
+    { id: "services", label: "Услуги", href: "/services", hasDropdown: true },
+    { id: "projects", label: "Проекти", href: "/projects" },
+    { id: "about", label: "За нас", href: "/about" },
+    { id: "contacts", label: "Контакти", href: "/contacts" },
 ];
