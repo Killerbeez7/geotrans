024b1675fac8763047e31dc366ce73959d500cfd
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2026-03-04 19:14:16 +0200

    remove: comments

--- geotrans-web/src/components/sections/ServiceSection.tsx
+++ geotrans-web/src/components/sections/ServiceSection.tsx
@@ -9,31 +9,6 @@ export const ServiceSection = ({ id, title, subtitle, items }: ServicesContent) 
 
   return (
     <section className="relative overflow-hidden bg-bg-page py-16 sm:py-20">
-      {/* Background pattern */}
-      {/* <div
-        aria-hidden
-        className="
-    pointer-events-none
-    absolute
-    left-1/2
-    top-0
-    -translate-x-1/2
-    w-[150%]
-    h-full
-    z-0
-    opacity-[0.80]
-    bg-[url('/patterns/vector.avif')]
-    bg-center
-    bg-cover
-  "
-        style={{
-          maskImage: "radial-gradient(circle at center, transparent 35%, black 75%)",
-          WebkitMaskImage:
-            "radial-gradient(circle at center, transparent 35%, black 75%)",
-        }}
-      /> */}
-
-      {/* Content */}
       <Section id={id} className="relative z-10" containerClassName="container-page">
         <header className="text-center">
           <h2 className="typo-h2">{title}</h2>
