bc67e3e634d77e342e0ee5323dadbe3ef03a6794
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2026-04-15 23:56:58 +0300

    redirect "/services" -> "/uslugi" to let crawlers scan

--- geoaxis-web/next.config.ts
+++ geoaxis-web/next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/services",
+        destination: "/uslugi",
+        permanent: true,
+      },
+      {
+        source: "/services/:path*",
+        destination: "/uslugi/:path*",
+        permanent: true,
+      },
+    ];
+  },
   reactCompiler: true,
 };
 
