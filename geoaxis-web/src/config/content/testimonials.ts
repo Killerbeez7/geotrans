e3313f3a3042d973e572cbd3aa1d54ab6e4c98ec
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2026-04-24 23:54:05 +0300

    fix typo

--- geoaxis-web/src/config/content/testimonials.ts
+++ geoaxis-web/src/config/content/testimonials.ts
@@ -8,7 +8,7 @@ export interface TestimonialsContent {
 export const testimonials: TestimonialsContent = {
   id: "testimonials",
   kicker: "Отзиви",
-  title: "Какво казват нашите клиентите",
+  title: "Какво казват нашите клиенти",
   subtitle:
     "Доверието на нашите клиенти е най-доброто доказателство за качеството на нашата работа.",
 } as const;
